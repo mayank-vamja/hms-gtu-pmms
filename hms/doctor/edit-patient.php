88481eb12d5e5e37facab1fbe386da45f7ca229a
Mayank Vamja <mayank.vamja@infostretch.com>
2020-04-01 17:17:34 +0530

    minor fixes

--- hms/doctor/edit-patient.php
+++ hms/doctor/edit-patient.php
@@ -1,25 +1,23 @@
+<?php include('include/header.php'); ?>
+
 <?php
 
-if(isset($_POST['submit']))
-{	
-	$eid=$_GET['editid'];
-	$patname=$_POST['patname'];
-$patcontact=$_POST['patcontact'];
-$patemail=$_POST['patemail'];
-$gender=$_POST['gender'];
-$pataddress=$_POST['pataddress'];
-$patage=$_POST['patage'];
-$medhis=$_POST['medhis'];
-$sql=mysqli_query($con,"update tblpatient set PatientName='$patname',PatientContno='$patcontact',PatientEmail='$patemail',PatientGender='$gender',PatientAdd='$pataddress',PatientAge='$patage',PatientMedhis='$medhis' where ID='$eid'");
-if($sql)
-{
-echo "<script>alert('Patient info updated Successfully');</script>";
-header('location:manage-patient.php');
-
-}
+if (isset($_POST['submit'])) {
+  $eid = $_GET['editid'];
+  $patname = $_POST['patname'];
+  $patcontact = $_POST['patcontact'];
+  $patemail = $_POST['patemail'];
+  $gender = $_POST['gender'];
+  $pataddress = $_POST['pataddress'];
+  $patage = $_POST['patage'];
+  $medhis = $_POST['medhis'];
+  $sql = mysqli_query($con, "update tblpatient set PatientName='$patname',PatientContno='$patcontact',PatientEmail='$patemail',PatientGender='$gender',PatientAdd='$pataddress',PatientAge='$patage',PatientMedhis='$medhis' where ID='$eid'");
+  if ($sql) {
+    echo "<script>alert('Patient info updated Successfully');</script>";
+    header('location:manage-patient.php');
+  }
 }
 ?>
-<?php include('include/header.php');?>
 
 <div class="main-content">
   <div class="wrap-content container" id="container">
@@ -51,75 +49,67 @@ header('location:manage-patient.php');
                 <div class="panel-body">
                   <form role="form" name="" method="post">
                     <?php
- $eid=$_GET['editid'];
-$ret=mysqli_query($con,"select * from tblpatient where ID='$eid'");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
+                    $eid = $_GET['editid'];
+                    $ret = mysqli_query($con, "select * from tblpatient where ID='$eid'");
+                    $cnt = 1;
+                    while ($row = mysqli_fetch_array($ret)) {
 
-?>
-                    <div class="form-group">
-                      <label for="doctorname">
-                        Patient Name
-                      </label>
-                      <input type="text" name="patname" class="form-control" value="<?php  echo $row['PatientName'];?>"
-                        required="true">
-                    </div>
-                    <div class="form-group">
-                      <label for="fess">
-                        Patient Contact no
-                      </label>
-                      <input type="text" name="patcontact" class="form-control"
-                        value="<?php  echo $row['PatientContno'];?>" required="true" maxlength="10" pattern="[0-9]+">
-                    </div>
-                    <div class="form-group">
-                      <label for="fess">
-                        Patient Email
-                      </label>
-                      <input type="email" id="patemail" name="patemail" class="form-control"
-                        value="<?php  echo $row['PatientEmail'];?>" readonly='true'>
-                      <span id="email-availability-status"></span>
-                    </div>
-                    <div class="form-group">
-                      <label class="control-label">Gender: </label>
-                      <?php  if($row['Gender']=="Female"){ ?>
-                      <input type="radio" name="gender" id="gender" value="Female" checked="true">Female
-                      <input type="radio" name="gender" id="gender" value="male">Male
-                      <?php } else { ?>
-                      <label>
-                        <input type="radio" name="gender" id="gender" value="Male" checked="true">Male
-                        <input type="radio" name="gender" id="gender" value="Female">Female
-                      </label>
-                      <?php } ?>
-                    </div>
-                    <div class="form-group">
-                      <label for="address">
-                        Patient Address
-                      </label>
-                      <textarea name="pataddress" class="form-control"
-                        required="true"><?php  echo $row['PatientAdd'];?></textarea>
-                    </div>
-                    <div class="form-group">
-                      <label for="fess">
-                        Patient Age
-                      </label>
-                      <input type="text" name="patage" class="form-control" value="<?php  echo $row['PatientAge'];?>"
-                        required="true">
-                    </div>
-                    <div class="form-group">
-                      <label for="fess">
-                        Medical History
-                      </label>
-                      <textarea type="text" name="medhis" class="form-control"
-                        placeholder="Enter Patient Medical History(if any)"
-                        required="true"><?php  echo $row['PatientMedhis'];?></textarea>
-                    </div>
-                    <div class="form-group">
-                      <label for="fess">
-                        Creation Date
-                      </label>
-                      <input type="text" class="form-control" value="<?php  echo $row['CreationDate'];?>"
-                        readonly='true'>
-                    </div>
+                    ?>
+                      <div class="form-group">
+                        <label for="doctorname">
+                          Patient Name
+                        </label>
+                        <input type="text" name="patname" class="form-control" value="<?php echo $row['PatientName']; ?>" required="true">
+                      </div>
+                      <div class="form-group">
+                        <label for="fess">
+                          Patient Contact no
+                        </label>
+                        <input type="text" name="patcontact" class="form-control" value="<?php echo $row['PatientContno']; ?>" required="true" maxlength="10" pattern="[0-9]+">
+                      </div>
+                      <div class="form-group">
+                        <label for="fess">
+                          Patient Email
+                        </label>
+                        <input type="email" id="patemail" name="patemail" class="form-control" value="<?php echo $row['PatientEmail']; ?>" readonly='true'>
+                        <span id="email-availability-status"></span>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label">Gender: </label>
+                        <?php if ($row['Gender'] == "Female") { ?>
+                          <input type="radio" name="gender" id="gender" value="Female" checked="true">Female
+                          <input type="radio" name="gender" id="gender" value="male">Male
+                        <?php } else { ?>
+                          <label>
+                            <input type="radio" name="gender" id="gender" value="Male" checked="true">Male
+                            <input type="radio" name="gender" id="gender" value="Female">Female
+                          </label>
+                        <?php } ?>
+                      </div>
+                      <div class="form-group">
+                        <label for="address">
+                          Patient Address
+                        </label>
+                        <textarea name="pataddress" class="form-control" required="true"><?php echo $row['PatientAdd']; ?></textarea>
+                      </div>
+                      <div class="form-group">
+                        <label for="fess">
+                          Patient Age
+                        </label>
+                        <input type="text" name="patage" class="form-control" value="<?php echo $row['PatientAge']; ?>" required="true">
+                      </div>
+                      <div class="form-group">
+                        <label for="fess">
+                          Medical History
+                        </label>
+                        <textarea type="text" name="medhis" class="form-control" placeholder="Enter Patient Medical History(if any)" required="true"><?php echo $row['PatientMedhis']; ?></textarea>
+                      </div>
+                      <div class="form-group">
+                        <label for="fess">
+                          Creation Date
+                        </label>
+                        <input type="text" class="form-control" value="<?php echo $row['CreationDate']; ?>" readonly='true'>
+                      </div>
                     <?php } ?>
                     <button type="submit" name="submit" id="submit" class="btn btn-o btn-primary">
                       Update
@@ -139,5 +129,5 @@ while ($row=mysqli_fetch_array($ret)) {
   </div>
 </div>
 <!-- start: FOOTER -->
-<?php include('include/footer.php');?>
+<?php include('include/footer.php'); ?>
 <!-- end: FOOTER -->
